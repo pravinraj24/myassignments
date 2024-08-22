6cfa482a2f9928d22c1de44de5515d341a77b500
pravinraj24 <pravinraj2407@gmail.com>
2024-08-22 12:57:38 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,34 @@
+package week1Day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+           String mobilename="Realme";
+           String model="x7 max 5g";
+           int price=35000;
+           float andriodversion=7.1f;
+           char typeofcable='C';
+           boolean ischargeravailable = true;
+           double sarnumber=12234.4123231;
+           
+           System.out.println("mobilename");
+           System.out.println("model");
+           System.out.println("price");
+           System.out.println("andriodversion");
+           System.out.println("is chargeravailable");
+           System.out.println("sarnumber");
+           
+           
+		System.out.println(mobilename+"\n"+model+"\n"+price+"\n"+andriodversion+"\n"+typeofcable+"\n"+ ischargeravailable +"\n"+sarnumber);
+           
+           
+	}	
+}
+           
+           
+        		   
+        		   
+           
+
+
+
